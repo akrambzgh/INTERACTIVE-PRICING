62de44d38e79c12866b23d8e046b9b1c384a2ed2
akrambzgh <akrambzgh04@gmail.com>
2022-01-08 00:00:43 +0100

    last commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 let rangeInput = document.querySelector(".range-input")
 let priceText = document.querySelector(".price")
 
-priceText.innerHTML = `<h1>$0.00 </h1> <span> / mouth</span>`
+priceText.innerHTML = `<h1>$16.00 </h1> <span> / mouth</span>`
 
 rangeInput.oninput = () => {
     priceText.innerHTML = `<h1>$${rangeInput.value}.00 </h1> <span> / mouth</span>`
